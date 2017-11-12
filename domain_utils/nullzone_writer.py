9f90a519dc79b0a5f2fca67763fab328157583b7
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2017-11-12 20:32:17 +0800

    refactored nullzone writer to separate class

---
+++ domain_utils/nullzone_writer.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+class NullzoneWriter:
+    """docstring for ConfigBuilder."""
+    def __init__(self, output_path, ipv4='127.0.0.1', ipv6=None, domain='example.org', host='dns'):
+        self.output_path = output_path
+        self.ipv4 = ipv4
+        self.ipv6 = ipv6
+        self.domain = domain
+        self.host = host
+
+    def export_to_file(self):
+        content = self.get_config_content()
+        self.write_to_file(content)
+
+    def get_config_content(self):
+        null_zone_lines = [
+            '$TTL   86400       ; one day',
+            '@      IN      SOA     {}.     hostmaster.{}. ('.format(self.domain, self.domain),
+            '       {}  ; serial number YYMMDDHH'.format(datetime.now().strftime("%Y%m%d%H")),
+            '       28800       ; refresh   8 hours',
+            '       7200        ; retry     2 hours',
+            '       864000      ; expire    10 days',
+            '       86400)      ; min ttl   1 day',
+            '               NS      {}.{}.'.format(self.host, self.domain),
+            '               A       {}'.format(self.ipv4),
+            '*      IN      A       {}'.format(self.ipv4),
+            '       IN      AAAA    {}'.format(self.ipv6) if self.ipv6 else None,
+            '',
+        ]
+        null_zone_lines = [line for line in null_zone_lines if line is not None]
+
+        return '\n'.join(null_zone_lines)
+
+    def write_to_file(self, content):
+        file_name = self.output_path
+
+        output_file = open(file_name, 'w')
+        output_file.write(content)
+        output_file.close()
+
+        print('Created [nullzone] file "{}"'.format(file_name))
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from datetime import datetime
 import settings
 from domain_utils.crawler import Crawler
 from domain_utils.blacklist_writer import BlacklistWriter
+from domain_utils.nullzone_writer import NullzoneWriter
+
 
 def create_null_zone():
     null_zone_file_path = settings.NULL_ZONE_FILE_PATH
     fqdn = settings.FQDN
-    ip6 = settings.IP6
-    ip4 = settings.IP4
+    ipv6 = settings.IP6
+    ipv4 = settings.IP4
     domain_parts = fqdn.split('.')
 
     # set blacklist server ip
@@ -20,26 +22,8 @@ def create_null_zone():
     else:
         domain = '.'.join(domain_parts)
 
-    null_zone_lines = [
-        '$TTL   86400       ; one day',
-        '@      IN      SOA     %s.     hostmaster.%s. (' % (domain, domain),
-        '       %s  ; serial number YYMMDDHH' % (datetime.now().strftime("%Y%m%d%H")),
-        '       28800       ; refresh   8 hours',
-        '       7200        ; retry     2 hours',
-        '       864000      ; expire    10 days',
-        '       86400)      ; min ttl   1 day',
-        '               NS      %s.%s.' % (host, domain),
-        '               A       %s' % (ip4),
-        '*      IN      A       %s' % (ip4)
-    ]
-    if ip6 and ip6 != '':
-        null_zone_lines.append('       IN	AAAA	%s' % (ip6))
-
-    null_zone_content = '\n'.join(null_zone_lines)
-    print(null_zone_content)
-    null_zone_file = open(null_zone_file_path, 'w')
-    null_zone_file.write(null_zone_content)
-    null_zone_file.close()
+    nullzone_writer = NullzoneWriter(null_zone_file_path, ipv4, ipv6, domain, host)
+    nullzone_writer.export_to_file()
 
 
 def update_badlist():
