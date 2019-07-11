b63daf822fde7308eea4430e2a2b15546ac2c77d
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2019-07-12 00:56:45 +0800

    adblock-v2 - make v2 images the default. also python alpine

--- dns/scripts/build.sh
+++ dns/scripts/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t ragibkl/adblock_dns:v2 .
-# docker push ragibkl/adblock_dns:latest
+docker build -t ragibkl/adblock_dns:latest .
--- dns/scripts/push.sh
+++ dns/scripts/push.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker push ragibkl/adblock_dns:v2
+docker push ragibkl/adblock_dns:latest
