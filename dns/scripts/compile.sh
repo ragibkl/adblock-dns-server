66aefaae63dd13d445a01e93484d3faebf8fb212
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2019-11-16 19:33:57 +0800

    cleanup image after compile list

--- dns/scripts/compile.sh
+++ dns/scripts/compile.sh
@@ -3,6 +3,6 @@
 touch templates/blacklist.zone
 
 docker pull ragibkl/adblock_compiler
-docker run -ti \
+docker run -ti --rm \
     -v $PWD/templates/blacklist.zone:/data/output.d/blacklist.zone \
     ragibkl/adblock_compiler
