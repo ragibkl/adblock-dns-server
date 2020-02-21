1ee8effaeee78e3fc70e99acbef00589a86a386f
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-02-21 18:48:18 +0800

    whitelist source

--- compiler_rust/src/service/config.rs
+++ compiler_rust/src/service/config.rs
@@ -11,17 +11,27 @@ pub struct SourceConfig {
 
 pub struct AppConfig {
     blacklist_src_path: String,
+    whitelist_src_path: String,
 }
 
 impl AppConfig {
     pub fn new() -> AppConfig {
         AppConfig {
             blacklist_src_path: "data/blacklist-src-urls.json".to_string(),
+            whitelist_src_path: "data/whitelist-src-urls.json".to_string(),
         }
     }
 
     pub fn get_blacklist_srcs(&self) -> Vec<SourceConfig> {
         let file_content: String = fs::read_to_string(&self.blacklist_src_path)
+            .unwrap()
+            .parse()
+            .unwrap();
+        serde_json::from_str(&file_content).unwrap()
+    }
+
+    pub fn get_whitelist_srcs(&self) -> Vec<SourceConfig> {
+        let file_content: String = fs::read_to_string(&self.whitelist_src_path)
             .unwrap()
             .parse()
             .unwrap();
