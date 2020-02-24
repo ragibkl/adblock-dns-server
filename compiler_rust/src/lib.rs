e38ec92bad8bf5f5d0cc2d3173f0c7172c6b8fa8
Ragib Badaruddin <ragib@seekasia.com>
2020-02-24 11:58:51 +0800

    removed rayon and parallelize some sections

--- compiler_rust/src/lib.rs
+++ compiler_rust/src/lib.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
 extern crate async_std;
-extern crate rayon;
 
 use std::collections::HashSet;
 
@@ -38,15 +37,23 @@ async fn fetch_list(urls: Vec<SourceConfig>) -> HashSet<String> {
 }
 
 pub async fn run(config: AppConfig) {
-    let blacklist_set = fetch_list(config.get_blacklist_srcs()).await;
-    let whitelist_set = fetch_list(config.get_whitelist_srcs()).await;
-    let overrides_set = fetch_list(config.get_overrides_srcs()).await;
+    let blacklist_urls = config.get_blacklist_srcs();
+    let whitelist_urls = config.get_whitelist_srcs();
+    let overrides_urls = config.get_overrides_srcs();
 
+    let blacklist_handle = tokio::spawn(async move { fetch_list(blacklist_urls).await });
+    let whitelist_handle = tokio::spawn(async move { fetch_list(whitelist_urls).await });
+    let overrides_handle = tokio::spawn(async move { fetch_list(overrides_urls).await });
+
+    let blacklist_set = blacklist_handle.await.unwrap();
+    let whitelist_set = whitelist_handle.await.unwrap();
     let domains: Vec<String> = blacklist_set
         .difference(&whitelist_set)
         .map(|s| s.clone())
         .collect();
-    let content =
-        hosts_writer::build_content(domains, overrides_set.into_iter().collect::<Vec<_>>());
+
+    let overrides_set = overrides_handle.await.unwrap();
+    let overrides_list = overrides_set.into_iter().collect::<Vec<_>>();
+    let content = hosts_writer::build_content(domains, overrides_list);
     hosts_writer::write_to_file(&config.get_output_path(), &content);
 }
