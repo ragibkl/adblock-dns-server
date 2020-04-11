51ad41d2c1251846c444c72bd03f0b1ed7cd5255
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-04-11 15:48:58 +0800

    minor fix

--- EXAMPLES/adblock-extra/compile.sh
+++ EXAMPLES/adblock-extra/compile.sh
@@ -7,4 +7,4 @@ docker run -ti --rm \
     -v $PWD/data/blacklist.d/extra_blacklist.hosts:/data/blacklist.d/__extra_blacklist.hosts \
     -v $PWD/data/whitelist.d/extra_whitelist.hosts:/data/whitelist.d/__extra_whitelist.hosts \
     -v $PWD/data/output.d/blacklist.zone:/data/output.d/blacklist.zone \
-    ragibkl/adblock_compiler
+    ragibkl/adblock_compiler:rust
