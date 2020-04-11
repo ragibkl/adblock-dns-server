5c26ab8a25fae786e751c58bd9321ce18a3411be
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-04-11 15:08:04 +0800

    build compiler using ubuntu image

--- compiler/scripts/build.sh
+++ compiler/scripts/build.sh
@@ -4,5 +4,5 @@ TAG=${1:-rust}
 
 cp -r ../data/. data/.
 
-docker build --pull -t ragibkl/adblock_compiler:$TAG .
+docker build -t ragibkl/adblock_compiler:$TAG .
 docker push ragibkl/adblock_compiler:$TAG
