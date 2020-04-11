cd520e666829c07315c15054d03ede3f4968dac4
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-04-11 13:31:43 +0800

    removed dns

--- dns/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./replace.sh
-
-cp config/named.conf.local /etc/bind/named.conf.local
-cp config/named.conf.options /etc/bind/named.conf.options
-cp config/null.zone.file /etc/bind/null.zone.file
-
-named -g -u named
--- dns/scripts/push.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-TAG=${1:-latest}
-docker push ragibkl/adblock_dns:$TAG
