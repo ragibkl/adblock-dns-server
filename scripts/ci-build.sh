af6342bfa5629e1dc7f3325a60f3916966be40d0
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-04-12 15:32:38 +0800

    simpler build script

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
 
-WORKDIR=$(pwd)
-BRANCH=$(git describe --tags --exact-match 2> /dev/null \
-  || git symbolic-ref -q --short HEAD \
-  || git rev-parse --short HEAD)
-
-TAG=latest
-if [ "$BRANCH" != master ]
-then
-    TAG=$BRANCH
-fi
-
-echo "TAG=$TAG"
-
-# cd $WORKDIR/adblock_http
-# ./scripts/build.sh $TAG
-# ./scripts/push.sh $TAG
-
-cd $WORKDIR/compiler
-./scripts/build.sh $TAG
-./scripts/push.sh $TAG
-
-sleep 10
-
-cd $WORKDIR/dns
-./scripts/compile.sh $TAG
-./scripts/build.sh $TAG
-./scripts/push.sh $TAG
+cd $WORKDIR/dns_default
+./scripts/build.sh
