9dfe3762b918c3e7a3796f8ca77e0c29fc758a83
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-04-12 16:15:29 +0800

    no tty

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 cd dns_default
-./scripts/build.sh
+
+# compile blacklist
+touch output.d/blacklist.zone
+docker pull ragibkl/adblock_compiler:rust
+docker run \
+    --rm \
+    -e TERM="xterm" \
+    -v $PWD/../data/:/data/ \
+    -v $PWD/output.d/blacklist.zone:/data/output.d/blacklist.zone \
+    ragibkl/adblock_compiler:rust
+
+# build image
+docker build --pull -t ragibkl/adblock_dns:ci-build .
+# docker tag ragibkl/adblock_dns:default ragibkl/adblock_dns:latest
+
+# push image
+docker push ragibkl/adblock_dns:ci-build
+# docker push ragibkl/adblock_dns:latest
