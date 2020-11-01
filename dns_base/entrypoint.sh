e069680c89b99bf27ed1e617e17fab4702ffc308
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-11-01 21:13:14 +0800

    minor fix

--- dns_base/entrypoint.sh
+++ dns_base/entrypoint.sh
@@ -6,6 +6,8 @@ cp config/named.conf.local /etc/bind/named.conf.local
 cp config/named.conf.options /etc/bind/named.conf.options
 cp config/null.zone.file /etc/bind/null.zone.file
 
+mkdir -p /logs
+touch /logs/rpz_log.txt
 chmod a+rwx -R /logs
 
 named -f -u named
