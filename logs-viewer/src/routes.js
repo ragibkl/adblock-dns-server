5fb2b7f387377bbe6a7b64213bb94a473a0200c2
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2020-11-01 22:38:39 +0800

    minor fix

--- logs-viewer/src/routes.js
+++ logs-viewer/src/routes.js
@@ -5,23 +5,23 @@ const pify = require('pify')
 const readFileAsync = pify(fs.readFile)
 const router = new Router()
 
-function getPattern (ip = '') {
-  if (ip.startsWith('::ffff:')) {
-    return ip.replace('::ffff:', '')
+function getIp (ctx) {
+  const requestIp = ctx.ip
+  if (requestIp.startsWith('::ffff:')) {
+    return requestIp.replace('::ffff:', '')
   }
-  return ip
+  return requestIp
 }
 
 function filter (ip, data) {
-  const pattern = getPattern(ip)
   return data
     .split('\n')
-    .filter(v => v.includes(`${pattern}#`))
+    .filter(v => v.includes(`${ip}#`))
     .join('\n')
 }
 
 router.get('/', async ctx => {
-  const { ip } = ctx.request
+  const ip = getIp(ctx)
   const data = await readFileAsync('/logs/rpz_log.txt', 'utf8')
   const filteredData = filter(ip, data)
   ctx.body = `ip = ${ip}\n\n${filteredData}`
