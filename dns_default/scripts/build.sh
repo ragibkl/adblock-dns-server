f1cf958cc062324da88e1fdc0201bad795105db2
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2021-03-20 17:48:32 +0800

    simpler dns build

--- dns_default/scripts/build.sh
+++ dns_default/scripts/build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# copy data files
-cp -rf ../data/ data
-
 # build image
-docker build --pull --no-cache -t ragibkl/adblock_dns:default .
+docker build --pull --no-cache \
+    -t ragibkl/adblock_dns:default \
+    -f ./Dockerfile ..
 docker tag ragibkl/adblock_dns:default ragibkl/adblock_dns:latest
 
 # push image
