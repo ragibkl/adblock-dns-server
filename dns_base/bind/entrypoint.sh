8068a7c830e7dfc44679aea7a1207c4f7cdccca2
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2021-05-08 18:51:18 +0800

    minor refactor

--- dns_base/bind/entrypoint.sh
+++ dns_base/bind/entrypoint.sh
@@ -8,12 +8,12 @@ IPV6="${IPV6:-::1}"
 FORWARDER_1="${FORWARDER_1:-8.8.8.8}"
 FORWARDER_2="${FORWARDER_2:-8.8.4.4}"
 
-cat templates/named.conf | \
+cat /etc/bind/named.conf.template | \
     sed s,'/%FORWARDER_1%/',"${FORWARDER_1}",g | \
     sed s,'/%FORWARDER_2%/',"${FORWARDER_2}",g > \
     /etc/bind/named.conf
 
-cat templates/null.zone | \
+cat /etc/bind/null.zone.template | \
     sed s,'/%TIMESTAMP%/',"${TIMESTAMP}",g | \
     sed s,'/%FQDN%/',"${FQDN}",g | \
     sed s,'/%IPV4%/',"${IPV4}",g | \
