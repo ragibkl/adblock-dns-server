fa385bebd9bacc8bf3c35ebc641e96c073a606c8
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2021-05-09 16:43:25 +0800

    compile using args

--- dns_base/compiler/src/configuration.rs
+++ dns_base/compiler/src/configuration.rs
@@ -3,15 +3,16 @@ use structopt::StructOpt;
 #[derive(StructOpt)]
 struct Cli {
     // The path to the file to read config
-    #[structopt(short = "f")]
-    config_file: Option<String>,
+    #[structopt(short = "f", default_value = "data/configuration")]
+    config_file: String,
 
     // The path to the file to output blacklist file
-    #[structopt(short = "o")]
-    output_path: Option<String>,
+    #[structopt(short = "o", default_value = "data/output.d/blacklist.zone")]
+    output_path: String,
 
     // The path to the file to resolve relative sources
-    workdir: Option<String>,
+    #[structopt(default_value = ".")]
+    workdir: String,
 }
 
 #[derive(serde::Deserialize, Debug, Clone)]
@@ -32,17 +33,12 @@ pub struct AppConfig {
 
 pub fn load_config() -> Result<AppConfig, config::ConfigError> {
     let args = Cli::from_args();
-    let config_file = args.config_file.unwrap_or("data/configuration".to_string());
-    let output_path = args
-        .output_path
-        .unwrap_or("data/output.d/blacklist.zone".to_string());
-    let workdir = args.workdir.unwrap_or(".".to_string());
-
     let mut app_config = config::Config::default();
+
     app_config
-        .merge(config::File::with_name(&config_file))?
-        .set("output_path", output_path)?
-        .set("workdir", workdir)?;
+        .merge(config::File::with_name(&args.config_file))?
+        .set("output_path", args.output_path)?
+        .set("workdir", args.workdir)?;
 
     app_config.try_into()
 }
