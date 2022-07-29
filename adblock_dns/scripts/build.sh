406872682e2b9a1cd86275b4715ec38c48829e89
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-07-30 06:53:28 +0800

    consolidate all tags

--- adblock_dns/scripts/build.sh
+++ adblock_dns/scripts/build.sh
@@ -5,4 +5,12 @@ WORK_DIR="$SCRIPT_DIR"/..
 cd $WORK_DIR
 
 docker build --pull -t ragibkl/adblock_dns:self-update .
+
+# tag others
+docker tag ragibkl/adblock_dns:self-update ragibkl/adblock_dns:default
+docker tag ragibkl/adblock_dns:self-update ragibkl/adblock_dns:latest
+
+# push tags
 docker push ragibkl/adblock_dns:self-update
+docker push ragibkl/adblock_dns:default
+docker push ragibkl/adblock_dns:latest
