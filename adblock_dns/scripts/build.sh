8fb9af7d2727c4046c9da9257d1124cf4c0f9638
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-07-30 06:08:48 +0800

    fix scripts paths

--- adblock_dns/scripts/build.sh
+++ adblock_dns/scripts/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --pull -t ragibkl/adblock_dns:base -f ./Dockerfile ..
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+WORK_DIR="$SCRIPT_DIR"/..
+cd $WORK_DIR
+
+docker build --pull -t ragibkl/adblock_dns:base .
 docker push ragibkl/adblock_dns:base
