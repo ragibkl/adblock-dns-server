175679906fbb40a8e3775f895ab437948982b167
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-07-30 06:47:39 +0800

    new self-update scripts

--- adblock_dns/entrypoint.sh
+++ adblock_dns/entrypoint.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 
-TIMESTAMP=$(date +"%Y%m%d%H")
-FQDN="${FQDN:-localhost.localdomain}"
-IPV4="${IPV4:-127.0.0.1}"
-IPV6="${IPV6:-::1}"
+export TIMESTAMP=$(date +"%Y%m%d%H")
+export FQDN="${FQDN:-localhost.localdomain}"
+export IPV4="${IPV4:-127.0.0.1}"
+export IPV6="${IPV6:-::1}"
+export CONFIG_URL="${CONFIG_URL:-https://raw.githubusercontent.com/ragibkl/adblock-dns-server/master/data/configuration.yaml}"
 
-cat /etc/bind/null.zone.template |
-	sed s,'/%TIMESTAMP%/',"${TIMESTAMP}",g |
-	sed s,'/%FQDN%/',"${FQDN}",g |
-	sed s,'/%IPV4%/',"${IPV4}",g |
-	sed s,'/%IPV6%/',"${IPV6}",g >/etc/bind/null.zone
+ENV_VARS=$(printf '${%s} ' $(env | sed 's/=.*//'))
+envsubst "$ENV_VARS" < /etc/bind/null.zone.template > /etc/bind/null.zone
 
 mkdir -p /logs
 touch /logs/rpz_log.txt
-chmod a+rwx -R /logs
+chmod a+rw -R /logs
 
-named -f -u named
+run_bind() {
+	named -f -u named
+}
+
+run_update() {
+	while true; do
+		echo "updating blacklist"
+		compiler \
+			-f $CONFIG_URL \
+			-o /etc/bind/blacklist.zone
+		echo "updating blacklist complete"
+
+		rndc reload
+		sleep 3600
+	done
+}
+
+PID_LIST=""
+
+run_bind &
+PID_LIST="$PID_LIST $!"
+
+run_update &
+PID_LIST="$PID_LIST $!"
+
+trap "kill $PID_LIST" SIGINT
+wait $PID_LIST
+exit 0
--- adblock_dns/scripts/build.sh
+++ adblock_dns/scripts/build.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 WORK_DIR="$SCRIPT_DIR"/..
 cd $WORK_DIR
 
-docker build --pull -t ragibkl/adblock_dns:base .
-docker push ragibkl/adblock_dns:base
+docker build --pull -t ragibkl/adblock_dns:self-update .
+docker push ragibkl/adblock_dns:self-update
