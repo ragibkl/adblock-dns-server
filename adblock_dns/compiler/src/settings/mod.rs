419b6057744cc0b41171ff9275a310fce444103b
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-09-09 16:51:36 +0800

    load config first

--- adblock_dns/compiler/src/settings/mod.rs
+++ adblock_dns/compiler/src/settings/mod.rs
@@ -11,8 +11,8 @@ pub use configuration::*;
 pub async fn load_config() -> Result<AppConfig, config::ConfigError> {
     let settings = ConfigBuilder::<AsyncState>::default()
         .set_default("output_path", "data/output.d/blacklist.zone")?
-        .add_source(CliSource::new())
         .add_async_source(ConfigSource::new())
+        .add_source(CliSource::new())
         .add_source(config::Environment::with_prefix("APP"))
         .build()
         .await?;
