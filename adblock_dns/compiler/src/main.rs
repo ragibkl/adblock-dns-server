7f6ef7b2c331e111a00db0943dd921a2d47d7329
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2022-12-20 15:23:18 +0800

    removed compiler codes

--- adblock_dns/compiler/src/main.rs
+++
@@ -1,7 +0,0 @@
-use compiler::{load_config, run};
-
-#[tokio::main]
-async fn main() {
-    let config = load_config().await.unwrap();
-    run(config).await;
-}
--- adblock_dns/compiler/src/service/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod blacklist;
-pub mod overrides;
-pub mod whitelist;
-
-pub mod hosts_writer;
-pub mod loader;
-pub mod parser;
