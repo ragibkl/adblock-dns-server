50272c7b2b81ec64ca60e7e4726d51c04c05a525
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2023-08-22 23:05:49 +0800

    Revert "nginx logs http proxy"
    This reverts commit fa0f483cbaa3bbdbfce5025ffa520ed9f0cfb2ad.

--- adblock_dnsdist_logs_nginx/app/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./replace.sh
-cp nginx.conf /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
--- adblock_dnsdist_logs_nginx/app/replace.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-UPSTREAM_URL="${UPSTREAM_URL:-http://wordpress}"
-
-cat nginx.template.conf \
-    | sed s,'${UPSTREAM_URL}',"${UPSTREAM_URL}",g \
-    | sed s,'${TLS_DOMAIN}',"${TLS_DOMAIN}",g \
-    > nginx.conf
